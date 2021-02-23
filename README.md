5c82633b47a50bd7d15d25b44406bc47a0b3a74a
xaaphrodite <79451877+xaaphrodite@users.noreply.github.com>
2021-02-23 19:42:44 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,51 @@
+#  xaaphrodite 
+![alt text](https://cdn.freebiesupply.com/logos/large/2x/java-2-logo-png-transparent.png)
+
+# Java Language
+      
+   Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. 
+It's a general-purpose programming language intended to let application developers write once, run anywhere (WORA), Java applications are typically compiled to bytecode that can run on any Java virtual machine (JVM) regardless of the underlying computer architecture. 
+The syntax of Java is similar to C and C++, but has fewer low-level facilities than either of them. 
+Java was originally developed by James Gosling at Sun Microsystems (which has since been acquired by Oracle) and released in 1995 as a core component of Sun Microsystems' Java platform. The original and reference implementation Java compilers, virtual machines, and class libraries were originally released by Sun under proprietary licenses. 
+As of May 2007, in compliance with the specifications of the Java Community Process, Sun had relicensed most of its Java technologies under the GNU General Public License. 
+Oracle offers its own HotSpot Java Virtual Machine, however the official reference implementation is the OpenJDK JVM which is free open source software and used by most developers and is the default JVM for almost all Linux distributions. 
+
+## Release Date
+
+As of September 2020, Java 8 and 11 are supported as Long Term Support (LTS) versions, and one later non-LTS version is supported. Major release versions of Java, along with their release dates:
+
+| Versions | Date |
+| --- | --- |
+| JDK-Beta | 1995 |
+| JDK-1.0 | January 23, 1996 |
+| JDK-1.1 | February 19, 1997 |
+| JDK-1.2 | December 8, 1998 |
+| JDK-1.3 | May 8, 2000 |
+| JDK-1.4 | February 6, 2002 |
+| JDK-5.0 | September 30, 2004 |
+| JDK-SE-6 | December 11, 2006 |
+| JDK-SE-7 | July 28, 2011 |
+| JDK-SE-8 | March 18, 2014 |
+| JDK-SE-9 | September 21, 2017 |
+| JDK-SE-10 | March 20, 2018 |
+| JDK-SE-11 | September 25, 2018 |
+| JDK-SE-12 | March 19, 2019 |
+| JDK-SE-13 | September 17, 2019 |
+| JDK-SE-14 | March 17, 2020 |
+| JDK-SE-15 | September 15, 2020 |
+
+Sun has defined and supports four editions of Java targeting different application environments and segmented many of its APIs so that they belong to one of the platforms. The platforms are:
+
+- Java Card for `smart-cards`.
+- Java Platform, Micro Edition `Java ME` – targeting environments with limited resources.
+- Java Platform, Standard Edition `Java SE` – targeting workstation environments.
+- Java Platform, Enterprise Edition `Java EE` – targeting large distributed enterprise or Internet environments.
+   
+## Example
+```java
+public class HelloWorld {
+      public static void main(String[] args) {
+      System.out.println("Hemllo World!"); // Prints the string to the console.
+    }
+}
+```
---
+++ database_prototype/src/com/project/Main.java
@@ -0,0 +1,166 @@
+package com.project;
+
+/*
+language	: Java
+author		: xaaphrodite
+date		: 20-02-2021
+github		: https://github.com/xaaphrodite
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import javax.swing.JFileChooser;
+
+public class Main {
+// Program Beginning----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------    
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Scanner Input = new Scanner(System.in);
+        String userInput;
+        boolean display = true;
+        
+        try {
+        	while (display) {
+                ClearScreen();
+                System.out.print("xaaphrodite database\n"
+                		+ "1. list databse\n"
+                		+ "2. add database\n"
+                		+ "3. edit database\n"
+                		+ "4. remove database\n"
+                		+ "5. exit\n");
+                
+                System.err.println("\nyour choice?");
+                userInput = Input.next();
+
+                switch (userInput) {
+                    case "1":
+                    	ClearScreen();
+                        System.out.println("---show database---");
+                        ListDatabase();
+                        break;
+                    case "2":
+                    	ClearScreen();
+                        System.out.println("---add database---");
+                        // cari data
+                        break;
+                    case "3":
+                    	ClearScreen();
+                        System.out.println("---edit database---");
+                        // tambah data
+                        break;
+                    case "4":
+                    	ClearScreen();
+                        System.out.println("---remove database---");
+                        // ubah data
+                        break;
+                    case "5":
+                        ClearScreen();
+                        System.out.println("---exit---");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.err.println("\nwrong choice");
+                        continue;
+                }
+
+                display = choice("continue");
+            }Input.close();
+		} catch (Exception error) {
+			// TODO: handle exception
+		}
+    }
+
+    private static boolean choice(String message){
+        Scanner Input = new Scanner(System.in);
+        System.out.print(""+message+"[Y/n]? ");
+        
+        String userInput = Input.next();
+
+        while(!userInput.equalsIgnoreCase("Y") && !userInput.equalsIgnoreCase("y") &&
+        	  !userInput.equalsIgnoreCase("N") && !userInput.equalsIgnoreCase("n")) {
+            System.err.println("wrong choice [Y/n]");
+            System.out.print("\n"+message+"[Y/n]? ");
+            userInput = Input.next();
+        } 
+        
+        return userInput.equalsIgnoreCase("y");
+    }
+
+    private static void ClearScreen(){
+        try {
+            if (System.getProperty("os.name").contains("Windows")){
+                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033\143");
+            }
+        } catch (Exception ex){
+            System.err.println("cant clear screen your PC is pootato!");
+        }
+	}
+     
+// Core Program----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------    
+    
+    private static void ListDatabase() throws IOException{
+        FileReader fInput;
+        BufferedReader bufferInput;
+      
+        try {
+            fInput = new FileReader("D:\\admin\\Integrated Development Environment\\workspace\\eclipse\\database\\src\\com\\project\\database.txt");
+            bufferInput = new BufferedReader(fInput);
+        } catch (Exception error){
+            System.err.println("File not found!");
+            return;
+        }
+
+
+        System.out.println("\n|No |\tYear              |\tAuthor          |     \tPublisher     |          \tTitle                           |");
+        System.out.println("+---+---------------------+---------------------+---------------------+-------------------------------------------------+");
+        
+        String data = bufferInput.readLine();
+        int nomorData = 0;
+        while(data != null) {
+            nomorData++;
+
+            StringTokenizer stringToken = new StringTokenizer(data, ",");
+
+            stringToken.nextToken();
+            System.out.printf("| %-2s", nomorData);
+            System.out.printf("| %-20s", stringToken.nextToken());
+            System.out.printf("| %-20s", stringToken.nextToken());
+            System.out.printf("| %-20s", stringToken.nextToken());
+            System.out.printf("| %-20s", stringToken.nextToken());
+            System.out.print("\n");
+
+            data = bufferInput.readLine();
+        }
+
+        System.out.println("+---+---------------------+---------------------+---------------------+-------------------------------------------------+");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
